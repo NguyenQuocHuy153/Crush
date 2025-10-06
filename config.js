52331b711613d190b0a046b8cd836334e2815fa0
NguyenQuocHuy153 <huylo456789@gmail.com>
2025-10-06 20:21:21 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
-    titleWeb: "Gửi crush của tớ",
-    introTitle: 'Crush yêu dấu',
+    titleWeb: "Gửi Loppy iu dấu",
+    introTitle: 'Loppy yêu dấu',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Thoát web là chấp nhận yêu tớ rồi nhé!!!',
+    title: 'Em có yêu anh không 🥰',
+    desc: 'Thoát web là yêu rồi nhé!!!',
     btnYes: 'Thích lắm <33',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao Py lại yêu a <3',
+    btnReply: 'Gửi cho PY <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay, mình qua đón đi chơi nha.',
+    mess: 'Anh biết mà 🥰. Yêu Py nhiều nhiều 😘😘',
+    messDesc: 'Tối mai đi nắc nha Py.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://facebook.com/dinhkaito' //link mess của các bạn. VD: /dinhkaito
 }
